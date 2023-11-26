e3dfed01631c3d05a83c31711ab526419e8c431e
Unji Cho <unjicho@Unjis-MacBook-Pro.local>
2023-11-26 16:08:33 -0500

    final

---
+++ index.js
@@ -0,0 +1,22 @@
+//Test Spec: for Multiplicaton
+//Except multiply (2,3) to be a number
+//Except multiply (2,3) to be equal to 6
+//Except negative multiplying -2 by 4 to result in -8
+
+
+//Test Spec: Zero Multiplication
+//Except multiply (0,5) to be a number
+//Except multiply (0,5) to be equal to 0
+
+//Test Spec: Basic Concatenation of Odds
+//Except concatOdds([2 4, 6, 8]) to be a string
+//Except concatOdds([2 ,4 ,6, 8]) to be equal to ""
+
+//Test Spec: Mixed Odd and Even Numbers
+//Except concatOdds([1 2, 3, 4, 5, 6]) to be a string
+//Except concatOdds([1, 2, 3, 4, 5, 6]) to be equal to "135"
+
+//Test Spec: String input Error:
+//Except attempting to multiply "a" by 3 to result in an error
+//Except attempting to multiply 2 by "b" to result in an error
+
